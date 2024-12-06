a7d8db32b1b427b8e54c959ea009b2ec9a48859a
binoverfl0w <arjanmarku02@gmail.com>
2024-12-06 20:34:23 +0100

    Day01 pt2

--- src/Day01.kt
+++ src/Day01.kt
@@ -2,9 +2,9 @@ import java.util.*
 import java.util.regex.Pattern
 import kotlin.math.abs
 
-fun main() {
-    val whiteSpacePattern = Pattern.compile("\\s+")
+val whiteSpacePattern: Pattern = Pattern.compile("\\s+")
 
+fun main() {
     val list1 = PriorityQueue<Int>()
     val list2 = PriorityQueue<Int>()
 
@@ -19,6 +19,26 @@ fun main() {
 
     while (!list1.isEmpty()) {
         total += abs(list1.poll() - list2.poll())
+    }
+
+    total.println()
+}
+
+fun main2() {
+    val occurrences = mutableMapOf<Int, Int>()
+    val list1 = mutableListOf<Int>()
+
+    readInput("input1").forEach {
+        val nums = it.split(whiteSpacePattern)
+
+        list1 += nums[0].toInt()
+        occurrences.merge(nums[1].toInt(), 1) { a, b -> a + b }
+    }
+
+    var total = 0
+
+    for (n in list1) {
+        total += n * occurrences.getOrDefault(n, 0)
     }
 
     total.println()
